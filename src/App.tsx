f56b62083923a2c520de628b3f1626c16e819194
Artem Lukianets <gramsvotn@gmail.com>
2021-09-27 14:43:21 +0300

    title and text components developed. added sass and react-textarea-autosize packages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './App.scss';
+import TextareaAutosize from 'react-textarea-autosize';
+import plus from '../src/assets/icons/plus.png'
+import dots from '../src/assets/icons/dots.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => {
+    return (
+        <main>
+            <section id={'menu'}>
+
+            </section>
+            <section id={'content'}>
+                <header>
+
+                </header>
+                <Document/>
+            </section>
+        </main>
+    )
 }
 
-export default App;
+const Document = () => {
+
+    return (
+        <div id={'document'}>
+            <div id={'document-content'}>
+                <Title/>
+                <Text/>
+            </div>
+        </div>
+    )
+}
+
+const Title = () => {
+    const [value, setValue] = useState('')
+
+    return (
+        <div className={'title-item'}>
+            <TextareaAutosize value={value} placeholder={'Untitled'} className={'title-field'} onChange={e => setValue(e.target.value)}/>
+        </div>
+    )
+}
+
+const Text = () => {
+    const [value, setValue] = useState('')
+
+    return (
+        <div className={'text-item'}>
+            <div className={'text-item-controls'}>
+                <div className={'add-item'}>
+                    <img alt={''} src={plus}/>
+                </div>
+                <div className={'move-item'}>
+                    <img alt={''} src={dots}/>
+                </div>
+            </div>
+            <TextareaAutosize value={value} placeholder={`Type '/' for commands`} className={'text-field'} onChange={e => setValue(e.target.value)}/>
+        </div>
+    )
+}
